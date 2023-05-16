4c0a5c504bcdee949128fc91c991ef43cd83d212
qdesjo <qdesjo@gmail.com>
2023-05-17 00:02:04 +0200

    Menu déroulant + revisitation de l'interface

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const chatbot_button = document.getElementById('chatbot');
 chatbot_button.addEventListener("click", function() {
     // ce qu'il faut mettre
 });
+
+document.getElementById("menu-button").addEventListener("click", function() {
+    var menuItems = document.getElementById("menu-items");
+    if (menuItems.style.display === "none") {
+      menuItems.style.display = "block";
+    } else {
+      menuItems.style.display = "none";
+    }
+  });
